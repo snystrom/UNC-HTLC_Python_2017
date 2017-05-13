0e5cf9225bfe3556d7d65821eb3d1ae0a03808fc
snystrom <snystrom@ad.unc.edu>
2017-05-13 19:00:58 -0400

    Began work on lesson 3. Wrote script for generating random fasta entries.

---
+++ 03_FileManipulation/makeRandomFasta.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import random
+random.seed(123) # Set seed for reproducibility.
+
+def randFasta(nSeq = 0, minLen = 10, maxLen = 100):
+    # Generates random DNA sequences of random length, outputs with fasta headers
+    bases = ['A', 'T', 'G', 'C']
+    headers = []
+    sequences = []
+    
+    for i in range(1, nSeq+1):
+        
+        seqLen = random.randint(minLen, maxLen)
+        randSequence = [random.choice(bases) for _ in range(1, seqLen)] # Generate random DNA sequence of random length
+        seqString = ''.join(randSequence) # Combine sequence into a string
+        header = '>Seq_{0}'.format(i)     # Generate unique header for each sequence
+        
+        headers.append(header)
+        sequences.append(seqString)
+        
+    output = [] 
+    for head, seq in zip(headers, sequences):
+        entry = head + '\n' + seq + '\n'
+        output.append(entry)
+        
+    return(output)
+   
+myDNA = randFasta(nSeq = 100, minLen = 50, maxLen = 250)
+
+with open('myDNA.fa', 'w') as fasta:
+    for line in myDNA:
+        fasta.write(line)
