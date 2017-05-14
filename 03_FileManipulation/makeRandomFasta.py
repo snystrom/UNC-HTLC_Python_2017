4cef539a12142c7d0a018a80670687255bc5b24b
Spencer Nystrom <snystrom@ad.unc.edu>
2017-05-14 12:05:56 -0400

    Made new function to generate random fasta from PWM input. Will generate file and have students compute PWM?

--- 03_FileManipulation/makeRandomFasta.py
+++ 03_FileManipulation/makeRandomFasta.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import random
+import numpy as np
 random.seed(123) # Set seed for reproducibility.
 
 def randFasta(nSeq = 0, minLen = 10, maxLen = 100):
@@ -25,14 +26,53 @@ def randFasta(nSeq = 0, minLen = 10, maxLen = 100):
         
     return(output)
 
+def makeRandomFastaFromPWM(pwm_model, nSeq = 1):
+    bases = ['A', 'T', 'G', 'C']
+    sequences = []
+    headers = []
+   # take nsequences and run loop foreach one 
+    for i in range(1, nSeq+1):
+        seq = [] 
+        for position in range(0, len(pwm_model)):
+            pBase = pwm_model[position]
+            base = np.random.choice(bases, p = pBase)
+            seq.append(base)
+            
+        sequence = ''.join(seq)
+        headerString = '>Seq_{0}'.format(i)
+        
+        sequences.append(sequence)
+        headers.append(headerString)
+        
+    output = []
+    for head, seq in zip(headers, sequences):
+        entry = head + '\n' + seq + '\n'
+        output.append(entry)
+    return(output)
+
 def writeArrayToFile(array, fileName):
     with open(fileName, 'w') as outfile:
         for entry in array:
             outfile.write(entry)
-
-
-myDNA = randFasta(nSeq = 100, minLen = 50, maxLen = 250)
-writeArrayToFile(myDNA, 'myDNA.fa')
-
-PWMset = randFasta(nSeq = 100, minLen = 10, maxLen = 10)
-writeArrayToFile(PWMset, 'PWM.fa')
+           
+def Main():
+    myDNA = randFasta(nSeq = 100, minLen = 50, maxLen = 250)
+    writeArrayToFile(myDNA, 'myDNA.fa')
+    
+    
+    # PWM to generate sequences from
+    #         'A', 'T', 'G', 'C'
+    pBase1 = [0.8, 0.2, 0, 0]
+    pBase2 = [0.1, 0.2, 0.5, 0.2]
+    pBase3 = [0.1, 0.8, 0.05, 0.05]
+    pBase4 = [0.8, 0.1, 0.05, 0.05]
+    pBase5 = [0.2, 0.1, 0.2, 0.5]
+    pBase6 = [0.2, 0.8, 0, 0]
+    
+    pwm_model = [pBase1, pBase2, pBase3, pBase4, pBase5, pBase6]
+    
+    PWMset = makeRandomFastaFromPWM(pwm_model, nSeq = 100)
+    writeArrayToFile(PWMset, 'PWM.fa')
+    
+if __name__ == "__main__":
+    Main()
