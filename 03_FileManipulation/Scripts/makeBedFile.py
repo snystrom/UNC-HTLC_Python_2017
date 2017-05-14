4100b87fbb9cab3f6f1524e807f76359f2c1f865
Spencer Nystrom <snystrom@ad.unc.edu>
2017-05-14 14:48:26 -0400

    Added chip-seq exercise

---
+++ 03_FileManipulation/Scripts/makeBedFile.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+import random
+import numpy as np
+
+random.seed(123)
+
+chrNames = ['chrX', 'chr2L', 'chr2R', 'chr3R', 'chr3L']
+chrStart = 1
+chrEnd = 120000000
+behavior = ['increasing', 'decreasing', 'static']
+pBehavior_unbound = [0.2, 0.2, 0.6]
+pBehavior_bound = [0.8, 0.1, 0.1]
+binding = ['bound', 'unbound']
+pBinding = [0.25, 0.75]
+
+def randomPeakType(chrNames, chrStart, chrEnd, behavior, binding):
+    ChIP_type = np.random.choice(binding, p = pBinding)
+    
+    if ChIP_type == 'bound':
+        peakType = np.random.choice(behavior, p = pBehavior_bound) 
+    else:
+        peakType = np.random.choice(behavior, p = pBehavior_unbound)
+    
+    chromosome = random.choice(chrNames)
+    start = random.randint(chrStart, chrEnd)
+    
+    if ChIP_type == 'bound':
+        width = random.randint(20, 100)
+    else:
+        width = random.randint(550, 1000)
+        
+    end = start + width
+
+    if end > chrEnd:
+        end = chrEnd
+
+
+    peak = [chromosome, str(start), str(end), peakType, ChIP_type]
+    return(peak)
+
+peaks = []
+for i in range(0,500):
+    peak = randomPeakType(chrNames, chrStart, chrEnd, behavior, binding)
+    line = '\t'.join(peak)
+    peaks.append(line)
+
+with open('../Data/chip-seq.bed', 'w') as outfile:
+    for peak in peaks:
+        line = peak + '\n'
+        outfile.write(line)
--- 03_FileManipulation/Scripts/makeRandomFasta.py
+++ 03_FileManipulation/Scripts/makeRandomFasta.py
@@ -1,78 +1,84 @@
 #!/usr/bin/python3
 import random
 import numpy as np
-random.seed(123) # Set seed for reproducibility.
 
 def randFasta(nSeq = 0, minLen = 10, maxLen = 100):
     # Generates random DNA sequences of random length, outputs with fasta headers
     bases = ['A', 'T', 'G', 'C']
     headers = []
     sequences = []
-    
+
     for i in range(1, nSeq+1):
-        
+
         seqLen = random.randint(minLen, maxLen)
         randSequence = [random.choice(bases) for _ in range(0, seqLen)] # Generate random DNA sequence of random length
         seqString = ''.join(randSequence) # Combine sequence into a string
         header = '>Seq_{0}'.format(i)     # Generate unique header for each sequence
-        
+
         headers.append(header)
         sequences.append(seqString)
-        
-    output = [] 
-    for head, seq in zip(headers, sequences):
-        entry = head + '\n' + seq + '\n'
-        output.append(entry)
-        
-    return(output)
 
-def makeRandomFastaFromPWM(pwm_model, nSeq = 1):
-    bases = ['A', 'T', 'G', 'C']
-    sequences = []
-    headers = []
-   # take nsequences and run loop foreach one 
-    for i in range(1, nSeq+1):
-        seq = [] 
-        for position in range(0, len(pwm_model)):
-            pBase = pwm_model[position]
-            base = np.random.choice(bases, p = pBase)
-            seq.append(base)
-            
-        sequence = ''.join(seq)
-        headerString = '>Seq_{0}'.format(i)
-        
-        sequences.append(sequence)
-        headers.append(headerString)
-        
     output = []
     for head, seq in zip(headers, sequences):
         entry = head + '\n' + seq + '\n'
         output.append(entry)
+
+    return(output)
+
+def makeRandomFastaFromPWM(pssm, nSeq = 1):
+    # Takes position weight matrix as input, generates given number of Fasta format sequences according to PWM
+    bases = ['A', 'T', 'G', 'C']
+    sequences = []
+    headers = []
+
+    for i in range(1, nSeq+1):
+        seq = []
+
+        for position in range(0, len(pssm)):
+            pBase = pssm[position]
+            base = np.random.choice(bases, p = pBase)
+            seq.append(base)
+
+        sequence = ''.join(seq)
+        header = '>Seq_{0}'.format(i)
+
+        sequences.append(sequence)
+        headers.append(header)
+
+    output = []
+    for head, seq in zip(headers, sequences):
+        entry = head + '\n' + seq + '\n'
+        output.append(entry)
+
     return(output)
 
 def writeArrayToFile(array, fileName):
     with open(fileName, 'w') as outfile:
         for entry in array:
             outfile.write(entry)
-           
-def Main():
+
+if __name__ == "__main__":
+
+    random.seed(123) # Set seed for reproducibility.
+
+    # Generate random fasta file
     myDNA = randFasta(nSeq = 100, minLen = 50, maxLen = 250)
-    writeArrayToFile(myDNA, 'myDNA.fa')
-    
-    
-    # PWM to generate sequences from
+    writeArrayToFile(myDNA, '../Data/myDNA.fa')
+
+
+    # PWM to generate sequences from:
+    # Each array is probability of occurrance of each basepair at a given position
     #         'A', 'T', 'G', 'C'
     pBase1 = [0.8, 0.2, 0, 0]
-    pBase2 = [0.1, 0.2, 0.5, 0.2]
+    pBase2 = [0.1, 0.1, 0.7, 0.1]
     pBase3 = [0.1, 0.8, 0.05, 0.05]
     pBase4 = [0.8, 0.1, 0.05, 0.05]
-    pBase5 = [0.2, 0.1, 0.2, 0.5]
+    pBase5 = [0.1, 0.1, 0.1, 0.7]
     pBase6 = [0.2, 0.8, 0, 0]
-    
-    pwm_model = [pBase1, pBase2, pBase3, pBase4, pBase5, pBase6]
-    
-    PWMset = makeRandomFastaFromPWM(pwm_model, nSeq = 100)
-    writeArrayToFile(PWMset, 'PWM.fa')
-    
-if __name__ == "__main__":
-    Main()
+
+    # Actual weight matrix
+    pssm = [pBase1, pBase2, pBase3, pBase4, pBase5, pBase6]
+
+    PWMset = makeRandomFastaFromPWM(pssm, nSeq = 100)
+    writeArrayToFile(PWMset, '../Data/PWM.fa')
+
